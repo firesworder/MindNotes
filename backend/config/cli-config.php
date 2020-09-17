1d5baa7e571077cd705725377323b16ae9e6cac5
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-18 02:21:55 +0400

    Рефакторинг cli-config.php, подключение use

--- backend/config/cli-config.php
+++ backend/config/cli-config.php
@@ -1,4 +1,8 @@
 <?php
+
+use App\EntityManagerContainer,
+    \Doctrine\ORM\Tools\Console\ConsoleRunner;
+
 require_once "bootstrap.php";
 
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(\App\EntityManagerContainer::getEntityManager());
+return ConsoleRunner::createHelperSet(EntityManagerContainer::getEntityManager());
