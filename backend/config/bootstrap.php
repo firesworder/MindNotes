9ffa5102f20826675b521be0dd8be69cdf1b1109
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-18 02:35:20 +0400

    Добавил вывод ошибок, если задана переменная окружения ENV=development

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -2,4 +2,6 @@
 require_once getenv('PROJECT_DIR') . "/vendor/autoload.php";
 use App\EntityManagerContainer;
 
+ini_set('display_errors', getenv('ENV') === 'development');
+
 new EntityManagerContainer();
