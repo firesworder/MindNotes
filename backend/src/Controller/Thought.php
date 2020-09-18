9f4292d84fae0abbcd4b8034f83fa228e80d3593
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-18 14:40:48 +0400

    Пустая реализация контроллера Thought

---
+++ backend/src/Controller/Thought.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class Thought extends AbstractController
+{
+    public function createThought(Request $request)
+    {
+
+    }
+
+    public function updateThought(Request $request)
+    {
+
+    }
+
+    public function deleteThought(Request $request)
+    {
+
+    }
+
+    public function getThoughtDetail(Request $request)
+    {
+
+    }
+
+    public function getThoughtList(Request $request)
+    {
+
+    }
+}
