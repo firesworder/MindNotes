b660ff807ad8f7c69e16ac3c75e4121462919d38
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-18 14:40:41 +0400

    Пустая реализация контроллера Tag

---
+++ backend/src/Controller/Tag.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class Tag
+{
+    public function createTag(Request $request)
+    {
+
+    }
+
+    public function updateTag(Request $request)
+    {
+
+    }
+
+    public function deleteTag(Request $request)
+    {
+
+    }
+
+    public function getTagList(Request $request)
+    {
+
+    }
+}
