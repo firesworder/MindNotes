32af64f2842f18796e73a6aaa9bde0381ad7d8a4
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-27 06:06:28 +0400

    MN-18 Пример для проверки vuex

--- frontend/vue/src/components/vue-router-demo/index.vue
+++ frontend/vue/src/components/vue-router-demo/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     I am demo vue-router!
+    {{testVar}}
   </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex';
+
 export default {
   name: 'vue-router-demo',
+  computed: {
+    ...mapState(['testVar']),
+  },
 };
 </script>
 
