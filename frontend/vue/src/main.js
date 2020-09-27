871618531f951ca1ee2c124054adad3da5d4eb00
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-27 06:10:50 +0400

    MN-18 (Рефакторинг) Изменил инициализацию vue-router

--- frontend/vue/src/main.js
+++ frontend/vue/src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import store from './store/index';
 import router from './router';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
 
 new Vue({
   vuetify,
--- frontend/vue/src/router.js
+++ frontend/vue/src/router.js
@@ -1,7 +1,10 @@
+import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 // Здесь подключаются компоненты для роутера, через .vue
 import demoComp from './components/vue-router-demo/index.vue';
+
+Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
