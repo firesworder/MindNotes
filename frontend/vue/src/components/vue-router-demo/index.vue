ffb3920ee9c29f410cfa175688cc681a4b4adef9
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-27 05:24:56 +0400

    MN-18 Создал пример для проверки работы vue-router

---
+++ frontend/vue/src/components/vue-router-demo/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    I am demo vue-router!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-router-demo',
+};
+</script>
+
+<style scoped>
+
+</style>
--- frontend/vue/src/router.js
+++ frontend/vue/src/router.js
@@ -1,9 +1,14 @@
 import VueRouter from 'vue-router';
 
 // Здесь подключаются компоненты для роутера, через .vue
+import demoComp from './components/vue-router-demo/index.vue';
 
 export default new VueRouter({
   routes: [
+    {
+      path: '/vue-router-demo/',
+      component: demoComp,
+    },
   ],
   mode: 'history',
 });
