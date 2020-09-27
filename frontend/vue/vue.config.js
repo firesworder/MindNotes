05f8a85f4d3c9819c6cfc3c324efa5d1c2c2542d
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-27 05:10:42 +0400

    MN-20 Заготовка для выполнения

--- frontend/vue/vue.config.js
+++ frontend/vue/vue.config.js
@@ -5,4 +5,10 @@ module.exports = {
   devServer: {
     proxy: 'http://localhost',
   },
+  // TODO: потом разобраться почему eslint это не берет(ошибка import/extensions)
+  // configureWebpack: {
+  //   resolve: {
+  //     extensions: ['.vue', '.js'],
+  //   },
+  // },
 };
