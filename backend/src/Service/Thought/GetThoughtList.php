0c7389d9b89935c86f0d040ec10f36f7b405296d
Nesterenko Ivan <ivanviktor93@gmail.com>
2020-09-30 09:37:12 +0400

    MN-24 Реализовал Service для получения списка мыслей

---
+++ backend/src/Service/Thought/GetThoughtList.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Service\Thought;
+
+
+use App\Entity\Thought;
+use App\Service\AbstractService;
+
+class GetThoughtList extends AbstractService
+{
+    /**
+     * Возвращает список мыслей
+     * @param array $params параметры вызова, по умолчанию []
+     * @return array Список мыслей [id,title,body,tags]
+     */
+    public function execute(array $params = [])
+    {
+        $result = [];
+        $repository = $this->entityManager->getRepository(Thought::class);
+        $thoughtList = $repository->findAll();
+
+        /** @var Thought $thought */
+        foreach ($thoughtList as $thought) {
+            $result[] = [
+                'id' => $thought->getId(),
+                'title' => $thought->getTitle(),
+                'body' => $thought->getBody(),
+                'tags' => [],
+                // TODO: Пока теги не реализованы - закомментировал
+//                'tags' => $thought->getTags(),
+            ];
+        }
+
+        return $result;
+    }
+}
